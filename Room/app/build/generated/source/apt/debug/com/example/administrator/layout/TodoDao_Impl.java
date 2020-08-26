6dc3816a90b25521aada7e279a0312ec46ab71fa
unknown <sangheon0724@gmail.com>
2020-08-26 23:48:38 +0900

    Room

---
+++ Room/app/build/generated/source/apt/debug/com/example/administrator/layout/TodoDao_Impl.java
@@ -0,0 +1,148 @@
+package com.example.administrator.layout;
+
+import android.database.Cursor;
+import androidx.lifecycle.LiveData;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Exception;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class TodoDao_Impl implements TodoDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<Todo> __insertionAdapterOfTodo;
+
+  private final EntityDeletionOrUpdateAdapter<Todo> __deletionAdapterOfTodo;
+
+  private final EntityDeletionOrUpdateAdapter<Todo> __updateAdapterOfTodo;
+
+  public TodoDao_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfTodo = new EntityInsertionAdapter<Todo>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `Todo` (`id`,`title`) VALUES (nullif(?, 0),?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Todo value) {
+        stmt.bindLong(1, value.getId());
+        if (value.getTitle() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getTitle());
+        }
+      }
+    };
+    this.__deletionAdapterOfTodo = new EntityDeletionOrUpdateAdapter<Todo>(__db) {
+      @Override
+      public String createQuery() {
+        return "DELETE FROM `Todo` WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Todo value) {
+        stmt.bindLong(1, value.getId());
+      }
+    };
+    this.__updateAdapterOfTodo = new EntityDeletionOrUpdateAdapter<Todo>(__db) {
+      @Override
+      public String createQuery() {
+        return "UPDATE OR ABORT `Todo` SET `id` = ?,`title` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Todo value) {
+        stmt.bindLong(1, value.getId());
+        if (value.getTitle() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getTitle());
+        }
+        stmt.bindLong(3, value.getId());
+      }
+    };
+  }
+
+  @Override
+  public void insert(final Todo todo) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __insertionAdapterOfTodo.insert(todo);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void delete(final Todo todo) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __deletionAdapterOfTodo.handle(todo);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void update(final Todo todo) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __updateAdapterOfTodo.handle(todo);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public LiveData<List<Todo>> getAll() {
+    final String _sql = "SELECT * FROM Todo";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return __db.getInvalidationTracker().createLiveData(new String[]{"Todo"}, false, new Callable<List<Todo>>() {
+      @Override
+      public List<Todo> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfTitle = CursorUtil.getColumnIndexOrThrow(_cursor, "title");
+          final List<Todo> _result = new ArrayList<Todo>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final Todo _item;
+            final String _tmpTitle;
+            _tmpTitle = _cursor.getString(_cursorIndexOfTitle);
+            _item = new Todo(_tmpTitle);
+            final int _tmpId;
+            _tmpId = _cursor.getInt(_cursorIndexOfId);
+            _item.setId(_tmpId);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+}
